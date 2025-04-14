d12dfbccf897732a18db0f5893dedefced79f660
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-04-14 13:32:43 +0330

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+// menu
+
+let overlay = document.getElementById('overlay');
+let openMenu = document.getElementById('openMenu');
+let mobileMenu = document.querySelector('.mobileMenu');
+
+openMenu.addEventListener('click', function () {
+  overlay.classList.add('active');
+  mobileMenu.classList.add('active');
+})
+overlay.addEventListener('click', function () {
+  overlay.classList.remove('active');
+  mobileMenu.classList.remove('active');
+})
+// swiper
+
+var testimonials = new Swiper(".testimonials", {
+  slidesPerView: 1,
+  spaceBetween: 26,
+  centeredSlides: true,   // فعال کردن حالت وسط چین
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1350: {
+      slidesPerView: 4,
+    }
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
