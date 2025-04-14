01d9a38ff3d156b85e37c64a69d475641f771824
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-04-14 16:29:58 +0330

    first commit

---
+++ src/js/slider.js
@@ -0,0 +1,152 @@
+// www.bytewebster.com
+// www.bytewebster.com
+// www.bytewebster.com
+const $ = str => document.querySelector(str);
+const $$ = str => document.querySelectorAll(str);
+
+(function() {
+    if (!window.app) {
+        window.app = {};
+    }
+    app.carousel = {
+        removeClass: function(el, classname='') {
+            if (el) {
+                if (classname === '') {
+                    el.className = '';
+                } else {
+                    el.classList.remove(classname);
+                }
+                return el;
+            }
+            return;
+        },
+        reorder: function() {
+            let childcnt = $("#carousel").children.length;
+            let childs = $("#carousel").children;
+
+            for (let j=0; j< childcnt; j++) {
+                childs[j].dataset.pos = j;
+            }
+        },
+        move: function(el) {
+            let selected = el;
+
+            if (typeof el === "string") {
+                console.log(`got string: ${el}`);
+                selected = (el == "next") ? $(".selected").nextElementSibling : $(".selected").previousElementSibling;
+                console.dir(selected);
+            }
+
+            let tgtpos = parseInt(selected.dataset.pos);
+            let curpos = parseInt($(".selected").dataset.pos);
+            let cnt = curpos - tgtpos;
+            let dir = (cnt < 0) ? -1 : 1;
+            let shift = Math.abs(cnt);
+
+            for (let i=0; i<shift; i++) {
+                let el = (dir == -1) ? $("#carousel").firstElementChild : $("#carousel").lastElementChild;
+
+                if (dir == -1) {
+                    el.dataset.pos = $("#carousel").children.length;
+                    $('#carousel').append(el);
+                } else {
+                    el.dataset.pos = 0;
+                    $('#carousel').prepend(el);
+                }
+
+                app.carousel.reorder();
+            }
+
+            app.selected = selected;
+            let next = selected.nextElementSibling;
+            var prev = selected.previousElementSibling;
+            var prevSecond = prev ? prev.previousElementSibling : selected.parentElement.lastElementChild;
+            var nextSecond = next ? next.nextElementSibling : selected.parentElement.firstElementChild;
+
+            selected.className = '';
+            selected.classList.add("selected");
+
+            app.carousel.removeClass(prev).classList.add('prev');
+            app.carousel.removeClass(next).classList.add('next');
+
+            app.carousel.removeClass(nextSecond).classList.add("nextRightSecond");
+            app.carousel.removeClass(prevSecond).classList.add("prevLeftSecond");
+
+            app.carousel.nextAll(nextSecond).forEach(item=>{ item.className = ''; item.classList.add('hideRight') });
+            app.carousel.prevAll(prevSecond).forEach(item=>{ item.className = ''; item.classList.add('hideLeft') });
+
+            // Reset auto-slide timer
+            app.carousel.resetAutoSlide();
+        },
+        nextAll: function(el) {
+            let els = [];
+
+            if (el) {
+                while (el = el.nextElementSibling) { els.push(el); }
+            }
+
+            return els;
+        },
+        prevAll: function(el) {
+            let els = [];
+
+            if (el) {
+                while (el = el.previousElementSibling) { els.push(el); }
+            }
+
+            return els;
+        },
+        keypress: function(e) {
+            switch (e.which) {
+                case 37: // left
+                    app.carousel.move('prev');
+                    break;
+
+                case 39: // right
+                    app.carousel.move('next');
+                    break;
+
+                default:
+                    return;
+            }
+            e.preventDefault();
+            return false;
+        },
+        select: function(e) {
+            console.log(`select: ${e}`);
+            let tgt = e.target;
+            while (!tgt.parentElement.classList.contains('carousel')) {
+                tgt = tgt.parentElement;
+            }
+
+            app.carousel.move(tgt);
+        },
+        previous: function(e) {
+            app.carousel.move('prev');
+        },
+        next: function(e) {
+            app.carousel.move('next');
+        },
+        startAutoSlide: function() {
+            app.carousel.autoSlideInterval = setInterval(() => {
+                app.carousel.move('next');
+            }, 4000); // 4 seconds
+        },
+        resetAutoSlide: function() {
+            clearInterval(app.carousel.autoSlideInterval);
+            app.carousel.startAutoSlide();
+        },
+        init: function() {
+            document.addEventListener("keydown", app.carousel.keypress);
+            // $("#carousel").addEventListener("click", app.carousel.select);
+
+            app.carousel.reorder();
+            app.selected = $(".selected");
+            
+            // Start auto-slide
+            app.carousel.startAutoSlide();
+        },
+        autoSlideInterval: null
+    }
+    app.carousel.init();
+})();
